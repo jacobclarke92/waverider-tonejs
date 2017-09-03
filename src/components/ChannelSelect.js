f494bc7f4beac11ef0747ebdaec45518e67aa23e
Jacob Clarke <jacobclarke92@gmail.com>
2017-09-04 01:48:14 +1000

    es2015 elegance

--- src/components/ChannelSelect.js
+++ src/components/ChannelSelect.js
@@ -5,7 +5,7 @@ export default class ChannelSelect extends Component {
 	render() {
 		const deviceOptions = [
 			{value: null, text: 'All Channels'},
-			...Array.apply(null, Array(16)).map((x, i) => ({value: i+1, text: 'Channel '+(i+1)}))
+			...Array(16).fill(0).map((x, i) => ({value: i+1, text: 'Channel '+(i+1)}))
 		]
 		return (
 			<SelectInput {...this.props} empty={false} defaultValue={null} options={deviceOptions} />
