ce228feccabc4799b12b9786d8173d1f0a94501f
Jacob Clarke <jacobclarke92@gmail.com>
2017-09-15 22:07:59 +1000

    Fixed wire math :D

--- src/components/desk/Wire.js
+++ src/components/desk/Wire.js
@@ -10,16 +10,14 @@ export default class Wire extends Component {
 		const fromPos = new Point(wireFrom.deskItem.position).add(new Point(wireFrom.relativePosition))
 		const toPos = wireTo ? new Point(wireTo.deskItem.position).add(new Point(wireTo.relativePosition)) : stagePointer
 		
-		const minX = Math.min(fromPos.x, toPos.x)
-		const maxX = Math.max(fromPos.x, toPos.x)
-		const minY = Math.min(fromPos.y, toPos.y)
-		const maxY = Math.max(fromPos.y, toPos.y)
-		const width = maxX - minX
-		const height = maxY - minY
-
+		// Get SVG width and height from point position difference
 		const diffX = toPos.x - fromPos.x
 		const diffY = toPos.y - fromPos.y
+		const width = Math.abs(diffX) || 1
+		const height = Math.abs(diffY) || 1
 		const vertical = width < height
+
+		// Get curve and bend amount based on position offsets
 		let curve = 2
 		let bend = 0
 		if(!vertical && diffX < 0) {
@@ -27,29 +25,42 @@ export default class Wire extends Component {
 			bend = diffY < 0 ? Math.max(diffY, 300)/500 : Math.min(diffY, 300)/500;
 		}
 
+		// Set SVG path start
+		const pathStart = {
+			x: fromPos.x < toPos.x ? 0 : width,
+			y: fromPos.y < toPos.y ? 0 : height
+		}
+
+		// Set SVG path end
+		const pathEnd = {
+			x: fromPos.x < toPos.x ? width : 0,
+			y: fromPos.y < toPos.y ? height : 0,
+		}
+
+		// Set bezier control point 1
 		const ctrlPt1 = {
-			x: vertical ? fromPos.x + diffX*bend : fromPos.x + diffX/curve,
-			y: vertical ? fromPos.y + diffY/curve : fromPos.y + diffY*bend
+			x: vertical ? pathStart.x + diffX*bend : pathStart.x + diffX/curve,
+			y: vertical ? pathStart.y + diffY/curve : pathStart.y + diffY*bend
 		}
 
+		// Set bezier control point 2
 		const ctrlPt2 = {
-			x: vertical ? toPos.x - diffX*bend : toPos.x - diffX/curve,
-			y: vertical ? toPos.y - diffY/curve : toPos.y - diffY*bend,
+			x: vertical ? pathEnd.x - diffX*bend : pathEnd.x - diffX/curve,
+			y: vertical ? pathEnd.y - diffY/curve : pathEnd.y - diffY*bend,
 		}
 
-		let path = `M0,0 C${ctrlPt1.x},${ctrlPt1.y} ${ctrlPt2.x},${ctrlPt2.y} ${width},${height}`
-		console.log(path)
+		const path = `M${pathStart.x},${pathStart.y} C${ctrlPt1.x},${ctrlPt1.y} ${ctrlPt2.x},${ctrlPt2.y} ${pathEnd.x},${pathEnd.y}`
 
 		const styles = {
 			width,
 			height,
-			left: minX,
-			top: minY,
+			left: Math.min(toPos.x, fromPos.x),
+			top: Math.min(toPos.y, fromPos.y),
 		}
 
 		return (
 			<svg className={classname('wire', {valid})} viewBox={`0 0 ${width} ${height}`} style={styles}>
-				<path d={path} />
+				<path d={path} strokeWidth={3} />
 			</svg>
 		)
 	}
