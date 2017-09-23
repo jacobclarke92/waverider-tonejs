7cf9d2e245c4e16f7b8346d625ea1d5d31050a0c
Jacob Clarke <jacobclarke92@gmail.com>
2017-09-23 15:19:50 +1000

    VU meter for master .. but not working yet

--- src/components/desk/Master.js
+++ src/components/desk/Master.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 import Icon from '../Icon'
+import VuMeter from './VuMeter'
 import DeskItemWrapper from './DeskItemWrapper'
 
 export default class MasterDeskItem extends Component {
 	render() {
+		const { type, ownerId } = this.props.deskItem || {}
 		return (
 			<DeskItemWrapper {...this.props}>
 				<div className="desk-item master" onMouseDown={e => console.log('MasterDeskItem mouseDown')}>
 					<Icon name="volume-up" size="large" />
-					<div className="vu-meter">
-
-					</div>
+					<VuMeter type={type} id={ownerId} />
 				</div>
 			</DeskItemWrapper>
 		)
