6fd98acd9f800a5aec47fa073b41aa6bcd6d0b5c
Jacob Clarke <jacobclarke92@gmail.com>
2019-03-09 19:51:03 +1000

    Add some key listeners -- unfinished

--- src/components/ui/PianoKey.tsx
+++ src/components/ui/PianoKey.tsx
@@ -1,13 +1,60 @@
 import React, { Component, CSSProperties } from 'react'
-import { ThunkDispatchProp } from '../../types'
+import { ThunkDispatchProp, GenericProps } from '../../types'
 import { connect } from 'react-redux'
 import cn from 'classnames'
 import { NOTE_ON, NOTE_OFF, MidiMessageAction } from '../../api/midi'
 import { internalPiano } from '../../reducers/devices'
+import { addKeyDownListener, addKeyUpListener, removeKeyDownListener, removeKeyUpListener } from '../../utils/keyUtils'
+
+/*
+0: C	a
+1: C#	w
+2: D	s
+3: Eb	e
+4: E	d
+5: F	f
+6: F#	t
+7: G	g
+8: Ab	y
+9: A	h
+10: Bb	u
+11: B	j
+*/
+
+const getNoteLetter = (note: number): string => {
+	if (note < 0 || note > 11) console.warn('relative note out of range')
+	switch (note) {
+		case 0:
+			return 'a'
+		case 1:
+			return 'w'
+		case 2:
+			return 's'
+		case 3:
+			return 'e'
+		case 4:
+			return 'd'
+		case 5:
+			return 'f'
+		case 6:
+			return 't'
+		case 7:
+			return 'g'
+		case 8:
+			return 'y'
+		case 9:
+			return 'h'
+		case 10:
+			return 'u'
+		case 11:
+			return 'j'
+	}
+}
 
 interface Props {
 	className: string
 	note: number
+	velocity: number
 	style: CSSProperties
 	octaveActive: boolean
 }
@@ -16,15 +63,45 @@ interface State {
 }
 
 class PianoKey extends Component<ThunkDispatchProp & Props, State> {
+	listening: boolean
+	noteLetter: string
 	static defaultProps = {
 		className: '',
 		note: 0,
+		velocity: 127,
 	}
-	state = { active: false }
+	constructor(props) {
+		super(props)
+		this.listening = false
+		this.noteLetter = getNoteLetter(props.note % 12)
+		this.triggerNoteDown = this.triggerNoteDown.bind(this)
+		this.triggerNoteUp = this.triggerNoteUp.bind(this)
+		this.state = {
+			active: false,
+		}
+	}
+	componentDidMount() {
+		this.autoBindListeners()
+	}
+	componentDidUpdate(prevProps) {
+		this.autoBindListeners(prevProps)
+	}
+	autoBindListeners(prevProps: GenericProps = {}) {
+		if (this.props.octaveActive && !prevProps.octaveActive) {
+			addKeyDownListener(this.noteLetter, this.triggerNoteDown)
+			addKeyUpListener(this.noteLetter, this.triggerNoteUp)
+		} else if (!this.props.octaveActive && prevProps.octaveActive) {
+			removeKeyDownListener(this.noteLetter, this.triggerNoteDown)
+			removeKeyUpListener(this.noteLetter, this.triggerNoteUp)
+		}
+	}
 	keyDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
 		e.preventDefault()
 		e.stopPropagation()
 		this.setState({ active: true })
+		this.triggerNoteDown()
+	}
+	triggerNoteDown() {
 		const { note, dispatch } = this.props
 		dispatch({ type: NOTE_ON, deviceId: internalPiano.id, channel: 1, note, velocity: 127 } as MidiMessageAction)
 	}
@@ -32,6 +109,9 @@ class PianoKey extends Component<ThunkDispatchProp & Props, State> {
 		e.preventDefault()
 		e.stopPropagation()
 		this.setState({ active: false })
+		this.triggerNoteUp()
+	}
+	triggerNoteUp() {
 		const { note, dispatch } = this.props
 		dispatch({ type: NOTE_OFF, deviceId: internalPiano.id, channel: 1, note, velocity: 127 } as MidiMessageAction)
 	}
