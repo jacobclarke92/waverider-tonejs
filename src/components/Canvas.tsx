b0242b1cad71896063297348a7db6727282cc4dd
Jacob Clarke <jacob@bigfish.tv>
2019-04-07 22:47:27 +1000

    Added basic canvas compoent that deals with pixel ratio displays and changes automatically

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+interface Props {
+	width: number
+	height: number
+	onReady: (ctx: CanvasRenderingContext2D, ref: HTMLCanvasElement) => void
+}
+
+interface State {
+	pixelRatio: number
+}
+
+export default class Canvas extends Component<Props, State> {
+	canvas: HTMLCanvasElement
+	ctx: CanvasRenderingContext2D
+	pixelRatioQuery: MediaQueryList
+
+	constructor(props: Props) {
+		super(props)
+		this.state = { pixelRatio: window.devicePixelRatio || 1 }
+		this.pixelRatioQuery = window.matchMedia('screen and (max-resolution: 1dppx)')
+	}
+
+	componentDidMount() {
+		this.pixelRatioQuery.addListener(this.handlePixelRatioChange)
+	}
+
+	componentWillUnmount() {
+		this.pixelRatioQuery.removeListener(this.handlePixelRatioChange)
+	}
+
+	handlePixelRatioChange = (e: MediaQueryListEvent) => {
+		this.setState({ pixelRatio: window.devicePixelRatio || 1 }, this.initCanvas)
+	}
+
+	handleRef = (ref: HTMLCanvasElement) => {
+		this.canvas = ref
+		this.ctx = this.canvas.getContext('2d')
+		this.initCanvas()
+		this.props.onReady(this.ctx, this.canvas)
+	}
+
+	initCanvas() {
+		const { pixelRatio } = this.state
+		const rect = this.canvas.getBoundingClientRect()
+		this.canvas.width = rect.width * pixelRatio
+		this.canvas.height = rect.height * pixelRatio
+		this.ctx.scale(pixelRatio, pixelRatio)
+	}
+
+	render() {
+		const { width, height } = this.props
+		return <canvas ref={this.handleRef} style={{ width, height }} />
+	}
+}
